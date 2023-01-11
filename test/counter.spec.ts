be630b1bece1c8a2172d760967d5d06e1e4a1461
shcube <shandin.va@gmail.com>
2023-01-11 13:07:53 +0700

    tests

--- test/counter.spec.ts
+++ test/counter.spec.ts
@@ -1,17 +1,33 @@
-import chai, {expect} from "chai";
+import chai, { expect } from "chai";
 import chaiBN from "chai-bn";
 import BN from "bn.js";
 import { beforeEach, describe } from "mocha";
 import { SmartContract } from "ton-contract-executor";
-import { beginCell, Cell } from "ton";
+import {
+  Address,
+  beginCell,
+  Cell,
+  CellMessage,
+  CommonMessageInfo,
+  InternalMessage,
+} from "ton";
 import { compileContract } from "ton-compiler";
 
 chai.use(chaiBN(BN));
 
 const INIT_COUNTER = 3;
 
+const zeroAddress = Address.parseRaw(
+  "0:0000000000000000000000000000000000000000000000000000000000000000"
+);
+
 function initData(initialCounterValue: number) {
   return beginCell().storeUint(initialCounterValue, 64).endCell();
+}
+
+function encodeIncrementMessage() {
+  const op = 1;
+  return beginCell().storeUint(op, 32).storeUint(0, 64).endCell();
 }
 
 describe("Counter tests", () => {
@@ -26,11 +42,32 @@ describe("Counter tests", () => {
     if (!compileResult.ok) throw new Error("compilation failed");
     const initCodeCell = Cell.fromBoc(compileResult.output)[0];
     const initDataCell = initData(INIT_COUNTER);
-    contract = await SmartContract.fromCell(initCodeCell, initDataCell);
+    contract = await SmartContract.fromCell(initCodeCell, initDataCell, {
+      debug: true,
+    });
   });
 
   it("should get init counter value", async () => {
     const call = await contract.invokeGetMethod("counter", []);
     expect(call.result[0]).to.be.bignumber.equal(new BN(INIT_COUNTER));
+  });
+
+  it("should increment counter", async () => {
+    const message = encodeIncrementMessage();
+    const send = await contract.sendInternalMessage(
+      new InternalMessage({
+        from: Address.parse("EQD4FPq-PRDieyQKkizFTRtSDyucUIqrj0v_zXJmqaDp6_0t"), // address of the sender of the message
+        to: zeroAddress, // ignored, this is assumed to be our contract instance
+        value: 0, // are we sending any TON coins with this message
+        bounce: true, // do we allow this message to bounce back on error
+        body: new CommonMessageInfo({
+          body: new CellMessage(message),
+        }),
+      })
+    );
+    expect(send.type).to.equal("success");
+    console.log(send.logs);
+    const call = await contract.invokeGetMethod("counter", []);
+    expect(call.result[0]).to.be.bignumber.equal(new BN(INIT_COUNTER + 1));
   });
 });
